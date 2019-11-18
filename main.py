c65952b30538f2a877fdd5d4ed9b0d1e6e311b8b
katiss <46756998+katiss@users.noreply.github.com>
2019-11-18 10:53:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from Employee import Employee 
 
 e=Employee("Michael","Janet","345-456-2219","E443")
+
+#list + strip files
+
+#find errors
+
+#define catorgories
+
+#print string
+
+#prompt user for file
