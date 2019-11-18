6a144be61157dcbd6d71b91b14127c1a52eb8ce2
katiss <46756998+katiss@users.noreply.github.com>
2019-11-18 10:58:04 -0500

    Update main.py
    Added user prompt

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ e=Employee("Michael","Janet","345-456-2219","E443")
 #print string
 
 #prompt user for file
+def main():
+  print("Enter 'F' to read employees from file, or 'I' to input directly,")
+  op=input("Or anything else to exit: ")
+  if op == 'F':
+    filename=input("Enter filename? ")
+    EmpList=loadRecs(filename)
+  elif op == 'I':
+    EmpList=inputRecs()
+  displayRecs(EmpList)
+    
+main()
